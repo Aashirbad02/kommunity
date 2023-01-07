9853ebe8c8bfe51ff865aeb34d88fa74399ebbbf
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-01-07 15:02:33 +0530

    Files changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import Navbar from "../Components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="text-5xl text-blue-600">Hello World</div>
+      <Navbar />
     </>
   );
 }
